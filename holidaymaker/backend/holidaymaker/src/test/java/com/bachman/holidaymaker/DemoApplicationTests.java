090cd55b87aab80e90e3e954fdc83881c51ccde0
antonohlin <55090787+antonohlin@users.noreply.github.com>
2020-04-08 10:23:02 +0200

    init

---
+++ holidaymaker/backend/holidaymaker/src/test/java/com/bachman/holidaymaker/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bachman.holidaymaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
